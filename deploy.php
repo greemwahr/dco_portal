c43a2580d74a8872bbc76b7af1d30bd4bd4c99aa
shadowsyntax <tibetianwolf@gmail.com>
2018-02-22 14:34:10 +0100

    Modified the passphrase in deploy.php

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@ define('SSH_USERNAME', 'dco1');
 // filename for the keypair to use -- no need to change this if you follow the readme instructions
 define('KEYPAIR_NAME', 'deploy');
 // the passphrase for your keypair
-define('KEYPAIR_PASSPHRASE', '');
+define('KEYPAIR_PASSPHRASE', 'ipnxnigeria');
 
 // END OF CONFIGURATION OPTIONS
 
